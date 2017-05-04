c9afd9f51df0436bc8f5f882ae100db1085968c8
correlation-one-datathon-team-5 <correlation-one-datathon-team-5@users.noreply.github.com>
2017-05-04 15:29:05 +0100

    Cleaned code

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, AffinityPropagation, MeanShift, SpectralClustering
+from sklearn.cluster import KMeans
 from scipy.cluster.hierarchy import linkage, dendrogram
-import pylab
+import seaborn as sns
 
-df = pd.read_csv('q5.csv', index_col=0)
+df = pd.read_csv('data.csv', index_col=0)
 
 remove_cols = []
 
@@ -13,6 +13,7 @@ for i in df.columns:
         remove_cols.append(i)
 
 df = df.drop(remove_cols, axis = 'columns')
+
 
 demographic_attributes = ['AGE IN YEARS', 'LOCATION',
        'BIRTH DATE', 'MEASUREMENT DATE', 'MEASUREMENT SET TP',
@@ -26,68 +27,119 @@ df = df.drop(demographic_attributes, axis = 'columns')
 
 print(df.loc[:,'AGE IN MONTHS'].describe())
 
-young_1 = df[(df.SEX == 1) & (df['AGE IN MONTHS'] < 168)]
-young_2 = df[(df.SEX == 2) & (df['AGE IN MONTHS'] >= 168)]
-old_1 = df[(df.SEX == 1) & (df['AGE IN MONTHS'] < 168)]
-old_2 = df[(df.SEX == 2) & (df['AGE IN MONTHS'] >= 168)]
+shoulder_data = df[['ERECT SITTING HEIGHT', 'AGE IN MONTHS']]
+shoulder_data['YEAR'] = shoulder_data['AGE IN MONTHS'].apply(lambda x: x//12)
+x = shoulder_data.groupby('YEAR').mean().index[1:-2]
+y = shoulder_data.groupby('YEAR').mean()['ERECT SITTING HEIGHT'][1:-2]
+
+sns.set_style("white")
+plt.rc('font', family='Raleway')
+
+plt.scatter(x, y, c='black', s=20)
+
+sns.despine()
+plt.xlabel('Age (Years)')
+plt.ylabel('Erect Sitting Height (mm)')
+plt.xlim([0, 21])
+plt.xticks([0, 5, 10, 15, 20])
+plt.show()
+
+
+shoulder_data = df[['ERECT SITTING HEIGHT', 'AGE IN MONTHS', 'SEX']]
+shoulder_data['YEAR'] = shoulder_data['AGE IN MONTHS'].apply(lambda x: x//12)
+y_1 = shoulder_data[shoulder_data['SEX'] == 1].groupby('YEAR').mean()['ERECT ' \
+                        'SITTING HEIGHT'][1:-2]
+y_2 = shoulder_data[shoulder_data['SEX'] == 2].groupby('YEAR').mean()['ERECT ' \
+                        'SITTING HEIGHT'][:-1]
+x = list(shoulder_data.groupby('YEAR').mean().index[1:-2])
+
+sns.set_style("white")
+plt.rc('font', family='Raleway')
+
+plt.scatter(x, y_1, c='blue', s=20)
+plt.scatter(x, y_2, c='red', s=20)
+
+plt.legend(labels=["Sex = 1 (Male?)", "Sex = 2 (Female?)"],
+               bbox_to_anchor=(0.05, .9, 0., 0.), loc=3, mode="expand",
+               borderaxespad=0., markerscale=1)
+
+sns.despine()
+plt.xlabel('Age (Years)')
+plt.ylabel('Erect Sitting Height (mm)')
+plt.xlim([0, 21])
+plt.xticks([0, 5, 10, 15, 20])
+plt.show()
+
+
+shoulder_data = df[['CHEST CIRCUMFERENCE', 'AGE IN MONTHS', 'SEX']]
+shoulder_data['YEAR'] = shoulder_data['AGE IN MONTHS'].apply(lambda x: x//12)
+y_1 = shoulder_data[shoulder_data['SEX'] == 1].groupby('YEAR').mean()['CHEST CIRCUMFERENCE'][1:-2]
+y_2 = shoulder_data[shoulder_data['SEX'] == 2].groupby('YEAR').mean()['CHEST CIRCUMFERENCE'][:-1]
+x = list(shoulder_data.groupby('YEAR').mean().index[1:-2])
+
+sns.set_style("white")
+plt.rc('font', family='Raleway')
+
+plt.scatter(x, y_1, c='blue', s=20)
+plt.scatter(x, y_2, c='red', s=20)
+
+plt.legend(labels=["Sex = 1 (Male?)", "Sex = 2 (Female?)"],
+               bbox_to_anchor=(0.05, .9, 0., 0.), loc=3, mode="expand",
+               borderaxespad=0., markerscale=1)
+
+sns.despine()
+plt.xlabel('Age (Years)')
+plt.ylabel('Chest Circumference (mm)')
+plt.xlim([0, 21])
+plt.xticks([0, 5, 10, 15, 20])
+plt.show()
 
 t_shirt_columns = ['CHEST CIRCUMFERENCE', 'WAIST CIRCUMFERENCE',
                    'SHOULDER-ELBOW LENGTH', 'ERECT SITTING HEIGHT',
                    'SHOULDER BREADTH']
 
-data = young_1.loc[:,t_shirt_columns].as_matrix()
+young = df[df['AGE IN MONTHS'] < 84].loc[:,t_shirt_columns]
+middle = df[(df['AGE IN MONTHS'] < 156) & (df['AGE IN MONTHS'] >= 83)].loc[:,t_shirt_columns]
+old_1 = df[(df.SEX == 1) & (df['AGE IN MONTHS'] >= 156)].loc[:,t_shirt_columns]
+old_2 = df[(df.SEX == 2) & (df['AGE IN MONTHS'] >= 156)].loc[:,t_shirt_columns]
 
-mergers = linkage(data, method='complete')
-dendrogram(mergers, labels = names, leaf_rotation=90, leaf_font_size=6)
-plt.title("young_1 Anthropometric T-shirt attributes Dendrogram")
-plt.show()
+drop_list = []
+for i in range(len(middle.index)):
+    if 0 in middle.ix[i,:].values:
+        drop_list.append(i)
 
+middle = middle.drop(middle.index[drop_list])
+
+data = middle.as_matrix()
+
+sns.set_style("white")
+plt.rc('font', family='Raleway')
 inertias = []
 ks = range(1,11)
 for k in ks:
     inertias.append(KMeans(n_clusters=k).fit(data).inertia_)
 plt.plot(ks, inertias, '-o')
 plt.xticks(ks)
+plt.xlabel('Number of Clusters')
+plt.ylabel('Total Inertia')
 plt.show()
 
-kmeans = KMeans(n_clusters=5).fit(data)
+
+mergers = linkage(data, method='complete')
+plt.figure(figsize=(20,10))
+sns.set_style("white")
+plt.rc('font', family='Raleway')
+dendrogram(mergers, leaf_rotation=90, leaf_font_size=6)
+plt.title("Middle Anthropometric T-shirt attributes Dendrogram")
+plt.show()
+
+kmeans = KMeans(n_clusters=4).fit(data)
 samples = kmeans.predict(data)
 
-x = data[:,0]
-y = data[:,1]
-plt.scatter(x, y, c=samples, cmap=pylab.cm.cool)
-plt.title("KMeans")
-centroids = kmeans.cluster_centers_
-centroid_x = centroids[:,0]
-centroid_y = centroids[:,1]
-plt.scatter(centroid_x, centroid_y, marker='D', s=50, c='red')
-plt.xlabel('CHEST CIRCUMFERENCE')
-plt.ylabel('WAIST CIRCUMFERENCE')
-plt.show()
-
-samples = AffinityPropagation().fit_predict(data)
-x = data[:,0]
-y = data[:,1]
-plt.scatter(x, y, c=samples, cmap=pylab.cm.cool)
-plt.title("Affinity Propagation")
-plt.xlabel('CHEST CIRCUMFERENCE')
-plt.ylabel('WAIST CIRCUMFERENCE')
-plt.show()
-
-samples = MeanShift().fit_predict(data)
-x = data[:,0]
-y = data[:,1]
-plt.scatter(x, y, c=samples, cmap=pylab.cm.cool)
-plt.title("Mean Shift")
-plt.xlabel('CHEST CIRCUMFERENCE')
-plt.ylabel('WAIST CIRCUMFERENCE')
-plt.show()
-
-names = young_1.index
+names = middle.index
 
 ns = pd.DataFrame({'names': names, 'cluster': samples})
 clusters = ns.set_index(names).iloc[:,0]
 
 print(clusters.value_counts())
-
 print(kmeans.cluster_centers_)
